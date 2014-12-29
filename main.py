c132152432c57e415e90def72e6df331baf1f911
Pablovm1990 <pablovm1990@gmail.com>
2014-12-29 11:22:40 +0100

    starting the main program

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Authors: Pablo Vicente Munuera and David Gómez Sánchez
+
+import sys
+import re
+import psycopg2 as dbi #importo un modulo y le doy un alias, importante para migrar codigo entre gestores de bases de datos
+
+'''
+Estas variables globales contienen los parametros de conexion a la base de datos, no necesariamente es bueno
+'''
+dbhost='localhost'	# El servidor, en este caso vuestro portatil
+dbname='masterdb'	# El nombre de la base de datos, que tendreis que cambiarlo
+dbuser='masteruser'	# Vuestro nombre de usuario
+dbpass='masterpass'	# La contrasenya para vuestro nombre de usuario NUNCA DEBERIAMOS PONER UNA CONTRASEÑA EN EL PROGRAMA
