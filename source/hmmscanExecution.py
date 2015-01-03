524d98e66505b6b03aa56ebac55d6ceb8b598a99
Pablovm1990 <pablovm1990@gmail.com>
2015-01-03 11:40:03 +0100

    explenation done

--- source/hmmscanExecution.py
+++ source/hmmscanExecution.py
@@ -3,4 +3,12 @@
 
 #Authors: Pablo Vicente Munuera and David Gómez Sánchez
 
-#Execution: python3 hmmscanExecution.py 
+#Execution: python3 hmmscanExecution.py
+
+'''
+Explanation: Programa que ejecute hmmscan sobre Pfam y las proteínas del organismo
+y procese el fichero de resultados generado por hmmscan para almacenar los resultados
+en la(s) tabla(s) del punto 1.c. Pero, en vez de usar todas las proteínas del organismo
+, se usarán solo aquellas secuencias cuyo tamaño total de secuencia, sea mayor a 
+la media del tamaño, de todas las secuencias del organismo.
+'''
