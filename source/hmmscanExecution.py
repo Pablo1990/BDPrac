c3b0ab428a15d69afe20b576e95f1e16ad9a4885
Pablovm1990 <pablovm1990@gmail.com>
2015-01-04 16:24:14 +0100

    starting hmmscan program

--- source/hmmscanExecution.py
+++ source/hmmscanExecution.py
@@ -12,3 +12,22 @@ en la(s) tabla(s) del punto 1.c. Pero, en vez de usar todas las proteínas del o
 , se usarán solo aquellas secuencias cuyo tamaño total de secuencia, sea mayor a 
 la media del tamaño, de todas las secuencias del organismo.
 '''
+
+'''
+hmmscan [-options] <hmmdb> <seqfile>
+
+Options controlling output:
+  -o <f>           : direct output to file <f>, not stdout
+  --tblout <f>     : save parseable table of per-sequence hits to file <s>
+  --domtblout <f>  : save parseable table of per-domain hits to file <s>
+  --pfamtblout <f> : save table of hits and domains to file, in Pfam format <s>
+  --acc            : prefer accessions over names in output
+  --noali          : don't output alignments, so output is smaller
+
+-----------------------------------------------------------------------------------------
+
+hmmpress - prepare an HMM database for hmmscan
+
+hmmpress [options] <hmmfile>
+'''
+
