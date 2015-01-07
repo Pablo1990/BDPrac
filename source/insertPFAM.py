df02a1ea25b4fd5bc922c39decab945f60b2ff86
Pablovm1990 <pablovm1990@gmail.com>
2015-01-07 16:47:06 +0100

    init insertPFAM

---
+++ source/insertPFAM.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Authors: Pablo Vicente Munuera and David Gómez Sánchez
+
+#Execution: python3.4 insertPFAM.py ../Datasets/Pfam-A.seed
+
+import sys
+import re
+import psycopg2 as dbi #importo un modulo y le doy un alias, importante para migrar codigo entre gestores de bases de datos
+
+'''
+Estas variables globales contienen los parametros de conexion a la base de datos, no necesariamente es bueno
+'''
+dbhost='localhost'	# El servidor, en este caso vuestro portatil
+dbname='masterdb'	# El nombre de la base de datos, que tendreis que cambiarlo
+dbuser='masteruser'	# Vuestro nombre de usuario
+dbpass='masterpass'	# La contrasenya para vuestro nombre de usuario NUNCA DEBERIAMOS PONER UNA CONTRASEÑA EN EL PROGRAMA
