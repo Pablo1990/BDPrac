d1e006e8de7f17c99757fef7ed54727ba6ec7942
Pablovm1990 <pablovm1990@gmail.com>
2015-01-20 13:11:06 +0100

    main done

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 #Execution: main.py
 
-from source import insertJGI, insertPFAM, hmmscanExecution, listaDomains
+from source import insertJGI, insertPFAM, hmmscanExecution, listaDomains, listFromInterpro, listLyasas
 import os.path
 
 print("Bienvenido/s a la práctica de BD\n")
@@ -17,7 +17,9 @@ while(option!='0') :
 	print("1 - Insertar datos desde JGI.")
 	print("2 - Insertar datos procedente PFAM.")
 	print("3 - Insertar datos desde HMMER3.")
-	print("4 - Lista info en función de un ")
+	print("4 - Lista dominio/s en función de un id o descripcion de un organismo.")
+	print("5 - Lista información referente a una referencia de Interpro.")
+	print("6 - Lista información de kinasas y junto con las lyasas, ion channel, transport y receptor.")
 	print("0 - Salir.")
 
 	option = input("Escoja una opción: ")
@@ -53,6 +55,14 @@ while(option!='0') :
 			print("Error: debe introducir algo.")
 		else :
 			listaDomains.main(inputString)
+	elif(option == '5'):
+		inputString = input("Inserte input a buscar: ")
+		if(inputString == '') :
+			print("Error: debe introducir algo.")
+		else :
+			listFromInterpro.main(inputString)
+	elif(option == '6'):
+		listLyasas.main()
 	elif(option == '0') :
 		print("Saliendo...")
 	else :
