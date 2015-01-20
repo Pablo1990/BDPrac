45d16e80c04b753c25de189672829fb904d2c816
Pablovm1990 <pablovm1990@gmail.com>
2015-01-20 13:49:55 +0100

    init.sh done

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 chmod a+x scripts/*
 
 mkdir temp
+
+echo localhost:5432:masterdb:masteruser:masterpass > ~/.pgpass
+
+chmod 0600 ~/.pgpass
+
+psql -h localhost masterdb masteruser -f source/sqlFiles/tablas.sql 
